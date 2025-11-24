08e78e0ef354e412e47b76b1c91989558ac950d4
Gavin Clarke <gavinclarke@Gavins-MacBook-Air.local>
2025-11-24 17:11:41 +0000

    Center hero layout and remove copy

--- react-app/src/components/HeroSection.jsx
+++ react-app/src/components/HeroSection.jsx
@@ -14,11 +14,6 @@ const HeroSection = () => {
             <em style={{ fontStyle: 'italic', textTransform: 'uppercase' }}>PLAY FOOTBALL.</em>
           </h1>
 
-          <div className="hero-copy">
-            <p>Football is at the heart of what we do. Young aspiring players are who we understand.</p>
-            <p>Based in Cheshire, all of our coaches have either played professionally or at academy level.</p>
-            <p>Our Mission is to pass on what we’ve learned, using a holistic approach to football coaching.</p>
-          </div>
         </div>
       </section>
     </>
